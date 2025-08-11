f0ab90e4092db40928936dd8693fae80a8bc22e3
narmada2601 <narmada2601@gmail.com>
2025-08-11 09:55:50 +0530

    ppractice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TestComponent } from './test-comp/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
   ],
   imports: [
     BrowserModule
